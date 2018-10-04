ce39f7e637d3a5417a8db30855f0ebcbafb32abd
Wabuluka <dwabuluka@gmail.com>
2018-10-04 14:44:43 +0300

    [chore] travis

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,10 +5,11 @@ class TestUser(BaseTest):
     def test_nothing(self):
         pass
 
-    def test_create_order(self):
+    def test_create_user(self):
         # testing the user sign in endpoint
-        response = self.client.post('/api/v1/auth/signin/1', json=dict(user))
+        response = self.client.post('/api/v1/auth/orders/1', json=dict(user))
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
 
 class UserTest(BaseTest):
